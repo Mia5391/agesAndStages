74c3e243eee439802ade691fdbf8212d10105e65
Mia5391 <mariakrasteva5391@gmail.com>
2018-05-28 12:39:40 -0400

    fixed flow error

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-let ageInput = (prompt("How old are you? (in years)"));
+let ageInput = (prompt("How old are you? (in complete years)"));
 
 isValidAge = function isInt(ageText){
     let inputIsANumber = Number(ageText) != NaN;
@@ -7,24 +7,27 @@ isValidAge = function isInt(ageText){
     return inputIsANumber && inputIsAnInteger;
   }
 
-  if (isValidAge(ageInput) && ageInput > 0 && ageInput <= 1 ) {
-        document.writeln  ('Congratulations, you are one VERY advanced baby!')
-      }
-  if (isValidAge(ageInput) && ageInput > 1 && ageInput <= 3 ) {
-        document.writeln ('Wow, what a talented toddler you are!')
-      }
-  if (isValidAge(ageInput) && ageInput > 3 && ageInput <= 5 ) {
+  if (isValidAge(ageInput)) {
+    if (ageInput > 0 && ageInput <= 1 ) {
+      document.writeln ('Congratulations, you are one VERY advanced baby!')
+    }
+    if (ageInput > 1 && ageInput <= 3 ) {
+      document.writeln ('Wow, what a talented toddler you are!')
+    }
+    if (ageInput > 3 && ageInput <= 5 ) {
         document.writeln ('Look at you, ready for preschool!')
-  }
-  if (isValidAge(ageInput) && ageInput > 5 && ageInput <= 12 ) {
-        document.writeln ('Off you go to gradeschool!')
-  }
-  if (isValidAge(ageInput) && ageInput > 12 && ageInput <= 18 ) {
-        document.writeln ('Welcome to Teenage Hell')
-  }
-  if (isValidAge(ageInput) && ageInput > 18 && ageInput <= 21 ) {
-    document.writeln ('Congratulations, you made it to young adulthood. No pressure or anything!')
+    }
+    if (ageInput > 5 && ageInput <= 12 ) {
+          document.writeln ('Off you go to gradeschool!')
+    }
+    if (ageInput > 12 && ageInput <= 18 ) {
+          document.writeln ('Welcome to Teenage Hell')
+    }
+    if (ageInput > 18 && ageInput <= 21 ) {
+      document.writeln ('Congratulations, you made it to young adulthood. No pressure or anything!')
+    }
+
   } else {
-    document.writeln ('Oops, you have to write an integer!')
+    document.writeln ('Write an integer')
   }
 };
